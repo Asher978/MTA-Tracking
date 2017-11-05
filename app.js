56e2fa256841e4ef9abcfdf3c7ae94805e130bd4
asher978 <ashershaheen@gmail.com>
2017-11-05 00:03:23 -0400

    server & middleware setup

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const ProtoBuf = require('protobufjs');
+
+const app = express();
+const server = require('http').Server(app);
+require('dotenv').config();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
+const port = process.env.PORT || 3001;
+server.listen(port, () => {
+  console.log(`Pumping on ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+;
+app.get('*', (req, res) => {
+  res.status(404).send('not found!');
+})
