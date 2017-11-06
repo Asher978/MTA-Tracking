dd56ec41f7ebaa615b3690c122902235603552d8
asher978 <ashershaheen@gmail.com>
2017-11-05 23:16:21 -0500

    created MTA component and added temporary fields for the api call

---
+++ client/src/components/Mta.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+const Mta = props => {
+  return (
+    <div>
+      <h3>This component is for test purposes ONLY!!</h3>
+      <p>Enter the number in the input field by the train of your choice from below...</p>
+      <li>1 for Train Lines - 1, 2, 3, 4, 5, 6, S</li>
+      <li>26 for Train Lines - A, C, E</li>
+      <li>16 for Train Lines - N, Q, R, W</li>
+      <li>21 for Train Lines - B, D, F, M</li>
+      <li>2 for Train Line - L</li>
+      <li>36 for Train Lines - J, Z</li>
+      <li>31 for Train Line - G</li>
+      <li>11 for Staten Island Railway</li>
+      <input type="number" onChange={props.handleOnChange} />
+      <input type="button" value="submit" onClick={props.handleMtaLookup} />
+    </div>
+  )
+}
+
+export default Mta;
