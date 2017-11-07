745418b63a537bc8bbd8e0cd2c227a680eab0073
asher978 <ashershaheen@gmail.com>
2017-11-06 23:49:29 -0500

    updated the train times message

--- client/src/components/TrainTimes.jsx
+++ client/src/components/TrainTimes.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TrainTimes = props => {
   return (
     <div>
-      <p>There are currently {props.mta.length} {props.train.toUpperCase()} trains</p>
+      <p>There are currently {props.mta.length} {props.train.toUpperCase()} trains running</p>
     </div>
   )
 }
