b209f57fa34f71e618a30f618e4a05666b9ab860
asher978 <ashershaheen@gmail.com>
2017-11-07 18:33:20 -0500

    native init commit

---
+++ nativeClient/App.js
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import MyTrain from './src/components/MyTain';
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <MyTrain />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default App;
