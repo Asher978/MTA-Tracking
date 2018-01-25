2ec67edc0fa74d5c3959e3c1dea9de47a0b6171b
asher978 <ashershaheen@gmail.com>
2018-01-25 17:55:14 -0500

    added react-redux - redux-thunk - provider - reducers - store

--- nativeClient/src/App.js
+++ nativeClient/src/App.js
@@ -1,32 +1,21 @@
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import MyTrain from './components/MyTrain';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
+import Router from './Router';
+import reducers from './reducers';
 
 class App extends Component {
   
-
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
     return (
-      <View style={styles.container}>
-        <MyTrain />
-      </View>
+      <Provider store={store}>
+        <Router />
+      </Provider>
     );
   }
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-});
 
 export default App;
