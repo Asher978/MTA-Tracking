0091d530b3c3b10a57316df5cdb643d7abd5f248
asher978 <ashershaheen@gmail.com>
2018-01-30 18:51:05 -0500

    created function for trainFetch

--- nativeClient/src/components/StopsList.js
+++ nativeClient/src/components/StopsList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text, ListView } from 'react-native';
 import Stop from './Stop';
 import { connect } from 'react-redux';
+// import { trainFetch } from '../actions';
 import _ from 'lodash';
 
 class StopsList extends Component {
@@ -18,9 +19,15 @@ class StopsList extends Component {
     return <Stop stop={stop} />;
   }
 
+  onButtonPress = (stop, feedId) => {
+
+    this.props.trainFetch(stop, feedId);
+  }
+
   render () {
     return (
     <ListView
+      enableEmptySections
       dataSource={this.dataSource}
       renderRow={this.renderRow}
     />
@@ -29,9 +36,10 @@ class StopsList extends Component {
 };
 
 const mapStateToProps = state => {
+  const { feedId } = state;
   
-  const stops = _.map(state.train.stops, (val) => {
-    return { ...val };
+  const stops = _.map(state.train, (val) => {
+    return { ...val, feedId };
   });
   
   return { stops };
